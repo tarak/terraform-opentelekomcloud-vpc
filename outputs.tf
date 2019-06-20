47c39701da8f0adf837d86e811690cc39c722704
Tarak Blah <halbkarat@gmail.com>
2019-06-20 15:36:23 +0200

    Fix outputs.

--- outputs.tf
+++ outputs.tf
@@ -5,5 +5,5 @@ output "vpc_id" {
 
 output "subnet_ids" {
   description = "List of IDs of subnets"
-  value       = var.enabled ? concat(opentelekomcloud_vpc_subnet_v1.this.*.id, [""])[0] : ""
+  value       = var.enabled ? opentelekomcloud_vpc_subnet_v1.this.*.id : [""]
 }
